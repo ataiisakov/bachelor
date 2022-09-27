dea4a7b38fc9d667c86d70ae3c4e9be5ca81d6aa
Ataibek Isakov <Ataibek.Isakov@flatexdegiro.com>
2022-09-27 17:56:30 +0200

    add background to image in detail view and make it gradient

--- app/src/main/java/com/example/bachelor/presentation/UserDetailsView.kt
+++ app/src/main/java/com/example/bachelor/presentation/UserDetailsView.kt
@@ -5,14 +5,13 @@ import android.graphics.*
 import android.os.Build
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.annotation.AttrRes
 import androidx.annotation.RequiresApi
 import androidx.constraintlayout.widget.ConstraintLayout
 import coil.load
 import coil.transform.CircleCropTransformation
 import com.example.bachelor.R
+import com.example.bachelor.databinding.UserDetailsViewBinding
 import com.example.bachelor.model.User
 import com.example.bachelor.utils.toPx
 
@@ -25,15 +24,13 @@ class UserDetailsView : ConstraintLayout {
     constructor(context: Context, attrs: AttributeSet?, @AttrRes defStyleAttr: Int)
             : super(context, attrs, defStyleAttr)
 
-    private lateinit var userPhoto: ImageView
-    private lateinit var userName: TextView
-    private lateinit var longText: TextView
+
+    private var _binding: UserDetailsViewBinding? = null
+    private val binding get() = _binding!!
 
     init {
-        LayoutInflater.from(context).inflate(R.layout.user_details_view, this, true)
-        userPhoto = findViewById(R.id.userPhotoImageView)
-        userName = findViewById(R.id.userNameTextView)
-        longText = findViewById(R.id.long_text)
+        val inflater = LayoutInflater.from(context)
+        _binding = UserDetailsViewBinding.inflate(inflater, this)
     }
 
     var user: User? = null
@@ -41,23 +38,28 @@ class UserDetailsView : ConstraintLayout {
             value ?: return
             field = value
 //          transition names needed for animation
-            userName.transitionName =
-                String.format(resources.getString(R.string.shared_image_transition), value.id)
-            userPhoto.transitionName =
-                String.format(resources.getString(R.string.shared_name_transition), value.id)
-//          set user properties
-            userName.text = value.name
-            userPhoto.load(value.photoUrl) {
-                crossfade(true)
-                transformations(CircleCropTransformation())
-                placeholder(R.drawable.ic_round_account_circle_56)
+            with(binding) {
+                userPhotoImageView.transitionName =
+                    String.format(resources.getString(R.string.shared_image_transition), value.id)
+                userNameTextView.transitionName =
+                    String.format(resources.getString(R.string.shared_name_transition), value.id)
+//          set user properties to views
+                userNameTextView.text = value.name
+                userPhotoImageView.load(value.photoUrl) {
+                    crossfade(true)
+                    transformations(CircleCropTransformation())
+                    placeholder(R.drawable.ic_round_account_circle_56)
+                }
+                longText.text = context.resources.getText(R.string.lorem_text)
             }
-            longText.text = context.resources.getText(R.string.lorem_text)
         }
 
-    private val profileRect = RectF()
+    private val bgBoundsRectF = RectF()
     private val path = Path()
-    private val FACTOR = 0.8
+    private val curveBgPath = Path()
+    private val photoOval = RectF()
+    private val FACTOR = 0.5
+    val handlePoint = PointF()
 
     private val paintPhotoBorder = Paint().apply {
         isAntiAlias = true
@@ -69,30 +71,25 @@ class UserDetailsView : ConstraintLayout {
     private val backgroundPaint = Paint().apply {
         isAntiAlias = true
         style = Paint.Style.FILL
-        color = Color.BLUE
+        color = resources.getColor(R.color.light_blue_bg_profile)
     }
 
+    private val curvedBgPaint = Paint().apply {
+        isAntiAlias = true
+        style = Paint.Style.FILL
+        color = resources.getColor(R.color.dark_blue_bg_profile)
+    }
 
     override fun dispatchDraw(canvas: Canvas) {
         drawPathBackground(canvas)
-//        drawPhotoBorder(canvas)
+        drawCurvedPathBackground(canvas)
+        drawPhotoBorder(canvas)
         super.dispatchDraw(canvas)
     }
 
-
-    private fun drawBackground(canvas: Canvas) {
-        val bgRight = width
-        val bgBottom = userPhoto.height * FACTOR
-        profileRect.apply {
-            top = 0f
-            left = 0f
-            right = bgRight.toFloat()
-            bottom = bgBottom.toFloat()
-        }
-        canvas.drawRect(profileRect, backgroundPaint)
-    }
-
     private fun drawPhotoBorder(canvas: Canvas) {
+        val userPhoto = binding.userPhotoImageView
+
         val photoRadius = userPhoto.width / 2f
         val cx = userPhoto.x + photoRadius
         val cy = userPhoto.y + photoRadius
@@ -100,31 +97,93 @@ class UserDetailsView : ConstraintLayout {
     }
 
     private fun drawPathBackground(canvas: Canvas) {
+        val userPhoto = binding.userPhotoImageView
+
         val photoRadius = userPhoto.width / 2f
         val centerPhotoX = userPhoto.x + photoRadius
-        val centerPhotoY = userPhoto.y + photoRadius
-        val photoBottom = (userPhoto.height * FACTOR).toFloat()
+        val photoBottom = userPhoto.y + (userPhoto.height).toFloat()
 
         val photoLeft = userPhoto.x
         val photoTop = userPhoto.y
         val photoRight = centerPhotoX + photoRadius
 
-        val oval = RectF(
-            photoLeft,
-            photoTop,
-            photoRight,
-            photoBottom
-        )
+        photoOval.apply {
+            left = photoLeft
+            top = photoTop
+            right = photoRight
+            bottom = photoBottom
+        }
+
+        val userBgHeight = (userPhoto.height * FACTOR).toFloat() + photoTop
+        bgBoundsRectF.apply {
+            left = this@UserDetailsView.left.toFloat()
+            top = this@UserDetailsView.top.toFloat()
+            right = this@UserDetailsView.right.toFloat()
+            bottom = userBgHeight
+        }
+
         path.apply {
-            moveTo(0f, 0f)
-            lineTo(0f, photoBottom)
-            lineTo(photoLeft, photoBottom)
-            arcTo(oval, -180f, 180f, false)
-            lineTo(right.toFloat(), photoBottom)
-            lineTo(right.toFloat(), 0f)
+            moveTo(bgBoundsRectF.left, bgBoundsRectF.top)
+            lineTo(bgBoundsRectF.left, bgBoundsRectF.bottom)
+            arcTo(photoOval, -180f, 180f, false)
+            lineTo(bgBoundsRectF.right, bgBoundsRectF.bottom)
+            lineTo(bgBoundsRectF.right, bgBoundsRectF.top)
             close()
         }
         canvas.drawPath(path, backgroundPaint)
+        path.reset()
+    }
+
+    private fun drawCurvedPathBackground(canvas: Canvas) {
+        val space = binding.space
+
+        val topCurvedBg = bgBoundsRectF.top + space.y + space.height.toFloat() / 3
+
+
+        handlePoint.apply {
+            x = bgBoundsRectF.left + (bgBoundsRectF.width() * 0.25f)
+            y = topCurvedBg
+        }
+        curveBgPath.apply {
+            moveTo(bgBoundsRectF.left, bgBoundsRectF.bottom)
+            arcTo(photoOval, -180f, 180f, false)
+            lineTo(bgBoundsRectF.right, bgBoundsRectF.bottom)
+            lineTo(bgBoundsRectF.right, topCurvedBg)
+            quadTo(
+                handlePoint.x, handlePoint.y,
+                bgBoundsRectF.left, bgBoundsRectF.bottom
+            )
+            close()
+        }
+        curvedBgPaint.shader = createRadialGradient()
+        canvas.drawPath(curveBgPath, curvedBgPaint)
+        curveBgPath.reset()
+    }
+
+    private fun createRadialGradient(): RadialGradient {
+
+        val colors = intArrayOf(
+            resources.getColor(R.color.light_blue_bg_profile),
+            resources.getColor(R.color.dark_blue_bg_profile)
+        )
+
+        val stops = floatArrayOf(
+            0.25f,
+            0.8f
+        )
+
+        val centerX = bgBoundsRectF.width() / 2f
+        val centerY = bgBoundsRectF.bottom
+
+        val radius = bgBoundsRectF.width() / 2f
+
+        return RadialGradient(
+            centerX, centerY,
+            radius,
+            colors,
+            stops,
+            Shader.TileMode.CLAMP
+        )
     }
 
 }
