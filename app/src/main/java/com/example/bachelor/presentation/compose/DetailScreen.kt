5024c5364daaf433b1df24229ce3199cf24d7c84
Ataibek Isakov <Ataibek.Isakov@flatexdegiro.com>
2022-10-18 17:47:02 +0200

    compose: add footer, header; detail view animation, update lib

--- app/src/main/java/com/example/bachelor/presentation/compose/DetailScreen.kt
+++ app/src/main/java/com/example/bachelor/presentation/compose/DetailScreen.kt
@@ -1,27 +1,34 @@
 package com.example.bachelor.presentation.compose
 
 import android.graphics.PointF
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddAPhoto
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.Dimension
+import androidx.constraintlayout.compose.*
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import coil.transform.CircleCropTransformation
@@ -29,6 +36,7 @@ import com.example.bachelor.R
 import com.example.bachelor.model.User
 import com.example.bachelor.presentation.theme.DarkBlue
 import com.example.bachelor.presentation.theme.LightBlue
+import kotlin.math.min
 
 
 @Composable
@@ -37,9 +45,14 @@ fun DetailScreen(user: User) {
 }
 @Composable
 fun UserDetailCard(user: User) {
+    val scroll = rememberScrollState(0)
+    val big = 200.dp
+    val small = 60.dp
+    val gap = with(LocalDensity.current) { big.toPx() - small.toPx() }
+    val progress = min(scroll.value / gap, 1f)
     ConstraintLayout(modifier = Modifier.fillMaxSize()) {
-        val (header, body) = createRefs()
-        Header(
+        val (header, body, fab) = createRefs()
+        HeaderMotion(
             user = user,
             modifier = Modifier
                 .constrainAs(header) {
@@ -47,59 +60,161 @@ fun UserDetailCard(user: User) {
                         top = parent.top,
                         start = parent.start,
                         end = parent.end,
-                        bottom = body.top
+                        bottom = body.top,
+                        verticalBias = 0f
                     )
                     width = Dimension.matchParent
-                    height = Dimension.value(250.dp)
+                },
+            progress = progress,
+            small = small,
+            big = big
+        )
+        Body(
+            modifier = Modifier
+                .padding(horizontal = 20.dp)
+                .constrainAs(body) {
+                    linkTo(
+                        top = header.bottom,
+                        start = parent.start,
+                        end = parent.end,
+                        bottom = parent.bottom
+                    )
+                    height = Dimension.fillToConstraints
+                    width = Dimension.matchParent
                 }
+                .verticalScroll(scroll)
         )
-        Body(modifier = Modifier
-            .padding(20.dp)
-            .constrainAs(body) {
-                linkTo(
-                    top = header.bottom,
-                    start = parent.start,
-                    end = parent.end,
-                    bottom = parent.bottom
-                )
-                height = Dimension.fillToConstraints
-                width = Dimension.matchParent
+
+        AnimatedVisibility(
+            modifier = Modifier
+                .constrainAs(fab) {
+                    bottom.linkTo(parent.bottom, 10.dp)
+                    end.linkTo(parent.end, 10.dp)
+                },
+            visible = (progress == 1f)
+        ) {
+            FloatingActionButton(
+                onClick = { /*TODO*/ },
+            ) {
+                Icon(imageVector = Icons.Default.AddAPhoto, contentDescription = null)
             }
-        )
+        }
     }
 }
 
 @Composable
-fun Header(user: User, modifier: Modifier = Modifier) {
-    ConstraintLayout(modifier = modifier) {
-        val (text, image, background) = createRefs()
-        UserDetailsBackground(modifier = Modifier
-            .constrainAs(background) {
+fun Body(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(text = stringResource(id = R.string.lorem_text))
+    }
+}
+
+
+@OptIn(ExperimentalMotionApi::class)
+@Composable
+fun HeaderMotion(user: User, modifier: Modifier = Modifier, progress: Float, small: Dp, big: Dp) {
+
+    val motionScene = MotionScene {
+        val img = createRefFor("profile_image")
+        val txt = createRefFor("profile_name")
+        val bg = createRefFor("profile_background")
+        val from = constraintSet {
+            constrain(img) {
                 linkTo(
-                    top = parent.top,
+                    start = bg.start,
+                    end = bg.end,
+                )
+                top.linkTo(txt.bottom, margin = 30.dp)
+                width = Dimension.value(100.dp)
+                height = Dimension.value(100.dp)
+            }
+            constrain(bg) {
+                linkTo(
                     start = parent.start,
                     end = parent.end,
+                    top = parent.top,
                     bottom = parent.bottom,
                     verticalBias = 0f
                 )
                 width = Dimension.matchParent
-                height = Dimension.value(200.dp)
+                height = Dimension.value(big)
             }
+            constrain(txt) {
+                linkTo(
+                    start = bg.start,
+                    end = bg.end,
+                    top = bg.top,
+                    bottom = img.top,
+                    topMargin = 80.dp
+                )
+                width = Dimension.wrapContent
+                height = Dimension.wrapContent
+            }
+        }
+        val to = constraintSet {
+            constrain(img) {
+                linkTo(
+                    top = bg.top,
+                    bottom = bg.bottom,
+                )
+                start.linkTo(bg.start, margin = 10.dp)
+                height = Dimension.value(60.dp)
+                width = Dimension.value(60.dp)
+
+            }
+            constrain(bg) {
+                linkTo(
+                    start = parent.start,
+                    end = parent.end,
+                    top = parent.top,
+                    bottom = parent.bottom,
+                    verticalBias = 0f
+                )
+                height = Dimension.value(small)
+                width = Dimension.matchParent
+            }
+            constrain(txt) {
+                linkTo(
+                    top = bg.top,
+                    bottom = bg.bottom,
+                )
+                end.linkTo(bg.end)
+                scaleX = 0.7f
+                scaleY = 0.7f
+            }
+        }
+        defaultTransition(
+            from = from,
+            to = to
+        ) {
+            onSwipe = OnSwipe(
+                direction = SwipeDirection.Up,
+                side = SwipeSide.Bottom,
+                anchor = bg
+            )
+            keyAttributes(txt) {
+                frame(85) {
+                    scaleX = 0.7f
+                    scaleY = 0.7f
+                }
+            }
+        }
+    }
+    MotionLayout(
+        modifier = modifier,
+        motionScene = motionScene,
+        progress = progress
+    ) {
+        UserDetailsBackground(
+            modifier = Modifier
+                .layoutId("profile_background")
         )
         Text(
             text = user.name,
             modifier = Modifier
-                .constrainAs(text) {
-                    linkTo(
-                        start = background.start,
-                        end = background.end,
-                        top = background.top,
-                        bottom = image.top,
-                        topMargin = 80.dp
-                    )
-                    width = Dimension.wrapContent
-                    height = Dimension.wrapContent
-                },
+                .layoutId("profile_name"),
             style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 30.sp)
         )
         AsyncImage(
@@ -107,29 +222,13 @@ fun Header(user: User, modifier: Modifier = Modifier) {
                 .data(user.photoUrl)
                 .crossfade(true)
                 .transformations(CircleCropTransformation())
+                .placeholder(R.drawable.ic_round_account_circle_56)
                 .build(),
             placeholder = painterResource(id = R.drawable.ic_round_account_circle_56),
             contentDescription = null,
             modifier = Modifier
-                .constrainAs(image) {
-                    linkTo(
-                        start = text.start,
-                        end = text.end,
-                    )
-                    top.linkTo(text.bottom, margin = 30.dp)
-                    width = Dimension.value(100.dp)
-                    height = Dimension.value(100.dp)
-                }
+                .layoutId("profile_image")
         )
-    }
-}
-
-@Composable
-fun Body(modifier: Modifier = Modifier) {
-    Column(modifier = modifier
-        .fillMaxSize()
-        .verticalScroll(rememberScrollState())) {
-        Text(text = stringResource(id = R.string.lorem_text))
     }
 }
 
@@ -176,13 +275,8 @@ fun UserDetailsBackground(modifier: Modifier = Modifier) {
 @Preview
 @Composable
 fun BackgroundPreview() {
-    UserDetailsBackground(modifier = Modifier.fillMaxSize())
-}
-
-//@Preview(uiMode = UI_MODE_NIGHT_YES)
-@Composable
-fun UserDetailCardPreview() {
     Surface {
         UserDetailCard(user = User(1, "Detail View", ""))
     }
 }
+
--- app/src/main/java/com/example/bachelor/presentation/compose/OverviewScreen.kt
+++ app/src/main/java/com/example/bachelor/presentation/compose/OverviewScreen.kt
@@ -12,8 +12,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import coil.transform.CircleCropTransformation
@@ -30,8 +32,29 @@ fun OverviewScreen(onUserClick: (User)->Unit = {}) {
 @Composable
 fun UserList(usersList: List<User>, onUserClick: (User) -> Unit) {
     LazyColumn(modifier = Modifier.fillMaxSize()) {
+        item {
+            HeaderFooterListItem("Header Text")
+        }
         items(items = usersList) { user: User ->
             UserListItem(user = user, onUserClick = onUserClick)
+        }
+        item {
+            HeaderFooterListItem("Footer Text")
+        }
+    }
+}
+
+@Composable
+fun HeaderFooterListItem(text: String) {
+    Card {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp)
+                .padding(6.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(style = TextStyle(fontSize = 30.sp), text = text)
         }
     }
 }
@@ -49,12 +72,13 @@ fun UserListItem(user: User, onUserClick: (User) -> Unit) {
             .build()
     )
 
-    Card(onClick = { onUserClick(user) }) {
+    Card(
+        onClick = { onUserClick(user) }
+    ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 16.dp, vertical = 8.dp),
-            verticalAlignment = Alignment.CenterVertically
+                .padding(horizontal = 16.dp, vertical = 8.dp)
         ) {
             Image(
                 painter = painter,
