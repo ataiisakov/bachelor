7da82f1a6af3a22fcfc9ca2f382162fca6522ce5
Ataibek Isakov <Ataibek.Isakov@flatexdegiro.com>
2022-11-15 17:53:29 +0100

    flutter: fix animation

--- app_flutter/lib/screens/detail.dart
+++ app_flutter/lib/screens/detail.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 import 'dart:ui';
 
 import 'package:app_flutter/widgets/profile_background.dart';
@@ -17,112 +16,112 @@ class DetailScreen extends StatefulWidget {
 }
 
 class _DetailScreenState extends State<DetailScreen> {
-  late ScrollController _scrollController;
-
-  double _bgHeight = 265;
-  double _imageRadius = 50.0;
-  double _textScale = 1.0;
-  double progress = 0.0;
-  double _imgX = 0;
-  double _imgY = 0;
-  double _textX = 0;
-  double _textY = 0;
-  double _opacity = 0;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _scrollController = ScrollController()
-        ..addListener(() {
-          progress = min(1.0, _scrollController.offset.toDouble() / _bgHeight);
-          setState(() {
-              _bgHeight = lerpDouble(200.0, 80.0, progress)!;
-              _textScale = lerpDouble(1.0, 0.75, progress)!;
-              _imageRadius = lerpDouble(50.0, 20.0, progress)!;
-              if(progress == 1) {
-                _opacity = 1;
-              } else {
-                _opacity = 0;
-              }
-          });
-        });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: _buildProfile(context));
-  }
-
-  Widget _buildProfile(BuildContext context) {
-    return Scaffold(
-      body: Container(
-          width: double.infinity,
-          alignment: Alignment.center,
-          child: Column(
-              children: [
-                _buildProfileHeader(context),
-                _buildProfileBody(),
-              ]
-          )
-      ),
-      floatingActionButton:  AnimatedOpacity(
-        duration: Duration(milliseconds: 500),
-        opacity: _opacity,
-        child: Icon(
-          Icons.camera,
-          size: 40,
-        ),
-      ),
-    );
-  }
-
-  Widget _buildProfileHeader(BuildContext context) {
-    var screenSize = MediaQuery.of(context).size;
-    return SizedBox(
-      height: _bgHeight,
-      child: Stack(
-        clipBehavior: Clip.none,
-        children: <Widget>[
-          CustomPaint(
-            painter: ProfileBackground(),
-            size: Size(screenSize.width, _bgHeight),
-          ),
-          Positioned(
-              left: screenSize.width / 2 - _imageRadius,
-              top: _bgHeight * 0.5,
-              child: Column(
-                children: [
-                  // Text(
-                  //   widget.user.name,
-                  //   style: const TextStyle(
-                  //       fontWeight: FontWeight.bold, fontSize: 33),
-                  //   textScaleFactor: _textScale,
-                  // ),
-                  Container(height: 15),
-                  CircleAvatar(
-                    backgroundImage: NetworkImage(widget.user.photoUrl),
-                    radius: _imageRadius,
-                  )
-                ],
-              )
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget _buildProfileBody() {
-    return Expanded(
-        child: SingleChildScrollView(
-          controller: _scrollController,
-          scrollDirection: Axis.vertical,
-            child: Container(
-              padding:
-                  const EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 100),
-              child: Text(lorem(paragraphs: 4, words: 500)),
+    Widget profile = CustomScrollView(
+      slivers: [
+        SliverPadding(
+            sliver: SliverPersistentHeader(
+              pinned: true,
+              delegate: _SliverAppBarDelegate(
+                  minHeight: 80.0, maxHeight: 200.0, user: widget.user),
             ),
-        )
+            padding: EdgeInsets.only(bottom: 60)),
+        SliverList(
+            delegate: SliverChildListDelegate([
+          Container(
+            padding: const EdgeInsets.only(left: 15, right: 15, bottom: 100),
+            child: Text(lorem(paragraphs: 4, words: 500)),
+          ),
+        ]))
+      ],
     );
+
+    return Scaffold(
+        body: Container(
+            width: window.physicalSize.width,
+            alignment: Alignment.center,
+            child: profile));
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate(
+      {required this.minHeight, required this.maxHeight, required this.user});
+
+  final double minHeight;
+  final double maxHeight;
+  final User user;
+  double imgRadius = 50.0;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    var progress = shrinkOffset / maxHeight;
+    var size = MediaQuery.of(context).size;
+    var width = size.width;
+    var height = size.height;
+    return Stack(
+      fit: StackFit.expand,
+      clipBehavior: Clip.none,
+      children: <Widget>[
+        Container(
+          height: lerpDouble(maxHeight + imgRadius + 10, minHeight, progress),
+        ),
+        CustomPaint(
+          painter: ProfileBackground(),
+          size: Size(double.infinity, maxHeight),
+        ),
+        Positioned(
+            left: lerpDouble(width / 2 - imgRadius, width - 100, progress),
+            top: lerpDouble(maxHeight - imgRadius * 2, minHeight / 2, progress),
+            child: Text(
+              user.name,
+              overflow: TextOverflow.clip,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+              textScaleFactor: lerpDouble(1, 0.65, progress),
+            )),
+        Positioned(
+            left: lerpDouble(width / 2 - imgRadius, 20, progress),
+            top: lerpDouble(maxHeight - imgRadius,
+                minHeight / 2 - imgRadius * 0.10, progress),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(user.photoUrl),
+              radius: lerpDouble(imgRadius, 20, progress),
+            )),
+        Positioned(
+            left: width - 80,
+            top: height - 80,
+            child: AnimatedOpacity(
+              duration: Duration(milliseconds: 300),
+              opacity: lerpDouble(0.0, 1.0, progress)!,
+              child: Container(
+                width: 50,
+                height: 50,
+                decoration:
+                    BoxDecoration(color: Colors.blue, shape: BoxShape.circle),
+                child: Icon(
+                  Icons.add_a_photo,
+                  size: 25,
+                ),
+              ),
+            )),
+      ],
+    );
+  }
+
+  @override
+  // TODO: implement maxExtent
+  double get maxExtent => maxHeight;
+
+  @override
+  // TODO: implement minExtent
+  double get minExtent => minHeight;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    // TODO: implement shouldRebuild
+    throw UnimplementedError();
   }
 }
